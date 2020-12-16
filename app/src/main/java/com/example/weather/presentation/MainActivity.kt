2ba110e763ed4d0114a3024cf1c5d9ea10efacc1
awasilik <aleksanderwasilik@gmail.com>
2020-12-16 22:03:20 +0100

    ui adjustments

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.weather.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weather.R
+import com.example.weather.presentation.recycler.WeatherAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        update()
+    }
+
+    private fun initView()
+    {
+        refreshButton.setOnClickListener { update() }
+
+        forecast_recycler.adapter = WeatherAdapter()
+        forecast_recycler.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    private fun update()
+    {
     }
 }
---
+++ app/src/main/java/com/example/weather/presentation/recycler/WeatherAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.weather.presentation.recycler
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weather.R
+import kotlinx.android.synthetic.main.weather_recycler_item.view.*
+
+class WeatherAdapter() : RecyclerView.Adapter<WeatherAdapter.ViewHolder>() {
+
+    val sampleList = listOf(
+        Pair(12, Uri.parse("http://openweathermap.org/img/wn/10d@2x.png")),
+        Pair(3, Uri.parse("http://openweathermap.org/img/wn/09d@2x.png")),
+        Pair(25, Uri.parse("http://openweathermap.org/img/wn/04n@2x.png"))
+    )
+
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(data : Pair<Int, Uri>)
+        {
+            view.temperatureItemText.text = data.first.toString()
+            view.weatherItemImage.setImageURI(data.second)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.weather_recycler_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(sampleList[position])
+    }
+
+    override fun getItemCount() = sampleList.size
+}
