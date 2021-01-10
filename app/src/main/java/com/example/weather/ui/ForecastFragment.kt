2eb643d93a80317f562986d0c65accf083457b59
awasilik <aleksanderwasilik@gmail.com>
2021-01-10 19:13:11 +0100

    expandable recycler

--- app/src/main/java/com/example/weather/ui/ForecastFragment.kt
+++ app/src/main/java/com/example/weather/ui/ForecastFragment.kt
@@ -6,26 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weather.databinding.ForecastFragmentBinding
-import com.example.weather.domain.model.DailyForecast
 import com.example.weather.ui.adapters.DailyForecastAdapter
-import com.example.weather.ui.adapters.DailyForecastClickListener
 import com.example.weather.viewmodel.ForecastViewModel
-import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.forecast_fragment.*
-import java.time.format.DateTimeFormatter
-import java.time.format.TextStyle
-import java.util.*
 
 @AndroidEntryPoint
-class ForecastFragment : Fragment(), DailyForecastClickListener{
+class ForecastFragment : Fragment() {
 
     private lateinit var binding: ForecastFragmentBinding
     private val viewModel: ForecastViewModel by viewModels()
-    private val dailyForecastAdapter = DailyForecastAdapter(this)
+    private val dailyForecastAdapter = DailyForecastAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,10 +41,6 @@ class ForecastFragment : Fragment(), DailyForecastClickListener{
         setupView()
     }
 
-    override fun itemSelected(forecast: DailyForecast) {
-        updateForecast(forecast)
-    }
-
     private fun setupObservers() {
         binding.viewmodel?.forecast?.observe(binding.lifecycleOwner!!) {
             dailyForecastAdapter.updateForecast(it) }
@@ -60,14 +49,5 @@ class ForecastFragment : Fragment(), DailyForecastClickListener{
     private fun setupView() {
         forecast_recycler.adapter = dailyForecastAdapter
         forecast_recycler.layoutManager = LinearLayoutManager(context)
-    }
-
-    private fun updateForecast(forecast: DailyForecast){
-        forecast_tv_weekDay.text = forecast.time.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
-        forecast_tv_date.text = forecast.time.format(DateTimeFormatter.ofPattern("dd.MM"))
-        forecast_tv_temperature_day.text = forecast.dayTemperature.toString()
-        forecast_tv_temperature_night.text = forecast.nightTemperature.toString()
-
-        Picasso.get().load(forecast.imageUrl).into(forecast_img_weather)
     }
 }
--- app/src/main/java/com/example/weather/ui/adapters/DailyForecastAdapter.kt
+++ app/src/main/java/com/example/weather/ui/adapters/DailyForecastAdapter.kt
@@ -12,19 +12,11 @@ import java.time.format.TextStyle
 import java.util.*
 import kotlin.collections.ArrayList
 
-interface DailyForecastClickListener{
-    fun itemSelected(forecast: DailyForecast)
-}
-
-class DailyForecastAdapter(private val clickListener: DailyForecastClickListener)
+class DailyForecastAdapter()
     : RecyclerView.Adapter<DailyForecastAdapter.ViewHolder>() {
 
     private var forecastDataList: List<DailyForecast> = ArrayList()
     private var selectedIndex = 0
-        set(value){
-            field = value
-            updateSelectedItem()
-        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -34,22 +26,28 @@ class DailyForecastAdapter(private val clickListener: DailyForecastClickListener
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(forecastDataList[position])
-        holder.itemView.setOnClickListener { selectedIndex = position }
+
+        holder.detailLayout.visibility = View.GONE
+
+        if (selectedIndex == position) {
+            holder.detailLayout.visibility = View.VISIBLE
+        }
+
+        holder.itemView.setOnClickListener {
+            selectedIndex = position
+            notifyDataSetChanged()
+        }
     }
 
     override fun getItemCount() = forecastDataList.size
 
     fun updateForecast(newForecastDataList: List<DailyForecast>) {
         forecastDataList = newForecastDataList;
-        notifyDataSetChanged()
-        updateSelectedItem()
-    }
-
-    private fun updateSelectedItem(){
-        clickListener.itemSelected(forecastDataList[selectedIndex])
     }
 
     inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val detailLayout = view.daily_lyt_details
+
         fun bind(data: DailyForecast) {
             view.daily_tv_date.text = data.time.format(DateTimeFormatter.ofPattern("dd.MM"))
             view.daily_tv_weekDay.text = data.time.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
